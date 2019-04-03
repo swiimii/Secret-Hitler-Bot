74ea1978c008f7b87368111180923b8bd8f066e9
Sam Scherer <swiimii962@gmail.com>
2019-04-03 12:13:48 -0400

    changed mybot and classes
    Classes is now more object - oriented, and actually works (go figure) mybot class is also apparently free of bugs.
    
    Now should support multiple servers running different game instances.
    
    Added ability to cleanup, which removes the server from the gameServer list, to remove finished games thus keep processing requirements low

--- classes.py
+++ classes.py
@@ -1,30 +1,42 @@
 import discord
 import random
 
-class secretHitlerGame:
-    serversRunningGame = []
-    players = []
-    fascists = []
-    liberals = []
-    events = []
+class shGame:
+    gameServers = []
 
 
-    def __init__(self,client,firstPlayer,server,startedChannel):
-        addPlayer[firstPlayer]
-        serversRunningGame += [server]
-        announceGame(channel)
+    # def __init__(self,client,firstPlayer,server,startedChannel):
+    #     shGame.gameServers += [self]
+    #     self.inProgress = True
+    #     self.channel = startedChannel
+    #     self.server = server
+    #     self.players = [firstPlayer]
+    #     self.fascists = []
+    #     self.liberals = []
+    #     self.events = []
+    #     self.client = client
+    def __init__(self,client,message,channel):
+        shGame.gameServers += [self]
+        self.inProgress = True
+        self.channel = channel
+        self.server = message.server
+        self.players = [message.author]
+        self.fascists = []
+        self.liberals = []
+        self.events = []
+        self.client = client
 
 
-    def addPlayer(self,player):
-        players += [player]
+    async def addPlayer(self,player):
+        self.players += [player]
 
-    def definePlayerRatios(self):
+    async def definePlayerRatios(self):
         fascistsNumber = len(players) // 2 - 1
         liberalsNumber = len(players) - fascistsNumber
+        # Randomly add a number of players to each team, according to these ratios
 
-    def announceGame(client, channel):
-        msg = """---------------------------------\n
-        A Secret Hitler game is starting! \n
-        Type !shb-join to join in! \n
-        ---------------------------------"""
-        await client.send_message(channel, msg)
+    async def announceGame(self,message):
+        announcemsg = """A Secret Hitler game is starting! \nType '!sh-join' to join in! """
+        await self.client.send_message(message.channel,announcemsg)
+        newgamemsg = """Welcome to Secret Hitler, \nOnce everybody's in, type \n'!sh-start' to start the game! """
+        await self.client.send_message(self.channel,newgamemsg)
--- mybot.py
+++ mybot.py
@@ -1,6 +1,5 @@
 import discord
-from classes import secretHitlerGame
-
+from classes import shGame
 
 client = discord.Client()
 
@@ -10,21 +9,44 @@ async def on_message(message):
     if message.author == client.user:
         return
 
+    # Check if the bot is working ---- Switch to "help" function?
     if message.content.startswith('!hello'):
         msg = 'Hello {0.author.mention}'.format(message)
         await client.send_message(message.channel, msg)
 
-    if message.content.startswith('!shb-start'):
-
+    # Start a game
+    if message.content.startswith('!sh-start'):
         server = message.author.server
-        everyone = discord.PermissionOverwrite(read_messages=False)
-        player = discord.PermissionOverwrite(read_messages=True)
-        await client.create_channel(server, 'secret-hitler-game', (server.default_role, everyone), (message.author, player))
-        channel = c.name == server.channels
-        secretHitlerGame(client, message.author, message.channel, server)
+        await shStart(message)
+
+    # Clean up empty servers, to allow the start of a new game
+    if message.content.startswith('!sh-cleanup'):
+        if message.author.server_permissions.administrator:
+            await shCleanup(message.author.server)
+            for game in shGame.gameServers:
+                if game.server == message.server:
+                    await client.delete_channel(game.channel)
+                    await client.send_message(message.channel, "Cleaned up!")
+
         # https://stackoverflow.com/questions/48141407/how-would-i-make-my-python-3-6-1-discord-bot-create-a-new-text-channel-in-a-serv
 
+async def shStart(message):
+    # start a game of secret hitler, and add it to the gameServers list
+    server = message.author.server
+    everyone = discord.PermissionOverwrite(read_messages=False)
+    player = discord.PermissionOverwrite(read_messages=True)
+    for c in server.channels:
+        if c.name == 'secret-hitler-game':
+            await client.delete_channel(c)
+            break
+    channel = await client.create_channel(server, 'secret-hitler-game', (server.default_role, everyone), (server.me, player))
+    thisGame = shGame(client, message, channel)
+    shGame.gameServers += [thisGame]
+    await thisGame.announceGame(message)
 
+async def shCleanup(server):
+    check = True
+    #remove all secret hitler game instances in the server which have non-recent last events
 
 @client.event
 async def on_ready():
@@ -32,7 +54,5 @@ async def on_ready():
     print(client.user.name)
     print(client.user.id)
     print('------')
-
-
 
 client.run('NTYyNDAwODUyMzY3OTAwNjg0.XKKPVA.DVtDmn-n_kzNcEEO9BeMKJX_WtA')  # 'Token'
