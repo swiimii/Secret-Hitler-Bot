e813884f783dae0668b1749baec043771fdd2ab8
Sam Scherer <swiimii962@gmail.com>
2019-04-09 15:28:34 -0400

    Started working on game mechanics

--- classes.py
+++ classes.py
@@ -1,42 +1,195 @@
 import discord
 import random
+import time
 
 class shGame:
-    gameServers = []
+    gameServers = [] # list of Game instances
+    gameTimeout = 600 # time before a game will be caught by cleanup
 
-
-    # def __init__(self,client,firstPlayer,server,startedChannel):
-    #     shGame.gameServers += [self]
-    #     self.inProgress = True
-    #     self.channel = startedChannel
-    #     self.server = server
-    #     self.players = [firstPlayer]
-    #     self.fascists = []
-    #     self.liberals = []
-    #     self.events = []
-    #     self.client = client
-    def __init__(self,client,message,channel):
-        shGame.gameServers += [self]
-        self.inProgress = True
-        self.channel = channel
+    def __init__(self,client,message, gamechannel):
+        #server instance variables
+        self.inProgress = False
+        self.channel = gamechannel
         self.server = message.server
-        self.players = [message.author]
+
+        #game instance variables
+        self.players = [Player(message.author)]
         self.fascists = []
         self.liberals = []
-        self.events = []
+        self.hitler = "none"
+        self.deadPlayers = []
+        self.president = "none"
+        self.chancellor = "none"
+        self.votes = "none"
+        self.governmentApproved = False
+
+        self.fascistsPolicies = 0
+        self.liberalPolicies = 0
+        self.chaosTracker = 0
+        self.policyPile = Pile()
+        self.discardPile = Pile()
+        shGame.gameServers += [self]
+        self.events = [("Game Created", time.time())]
         self.client = client
 
+    async def createGame(client,message):
+        server = message.author.server
+        everyone = discord.PermissionOverwrite(read_messages=False)
 
-    async def addPlayer(self,player):
-        self.players += [player]
+        bot = discord.PermissionOverwrite()
+        bot.send_message = True
+        bot.read_messages = True
+        #if there is a finished game or a sh channel from a crashed/closed bot, clean up
+        await shGame.shCleanup(client, message)
+        channel = await client.create_channel(server, 'secret-hitler-game', (server.default_role, everyone), (server.me, bot))
+        thisGame = shGame(client, message, channel)
+        shGame.gameServers += [thisGame]
+        await thisGame.announceGame(message)
+
+        #populate policy pile
+        for i in range(17): #0-16 - in a game of secret hitler, there are 6 liberal policies, and 11 fascist policies
+            if i < 6:
+                team = 1 # liberal
+            else:
+                team = 0 # fascist
+            self.policyPile.policies += Pile(team)
+        self.policyPile.shuffle()
+
+
+
+    async def startGame(self):
+        self.inProgress = True
+        self.events += ("Game Started", time.time())
+        #define player roles & notify those players
+        await assignPlayerRoles()
+
+        #select a president
+        self.president = self.players[randint(0,len(players)-1)]
+        await callForAction("Select Chancellor")
+
+    async def callForAction(self, reason):
+        #call a vote in self.channel according to reason
+        if reason == "Select Chancellor":
+            await self.client.send_message("{0.mention}, please select a chancellor.").format(self.president)
+        elif reason == "Vote On Government":
+            await self.client.send_message("Everyone, please vote on this government. Vote by sending y / n to this bot in a DM.")
+
+
+    async def receiveVote(message):
+        for game in shGame.gameServers:
+            if message.author in game.players:
+                True
+                # receive a vote in dms
+
+
+    async def assignPlayerRoles(self):
+        #assign team ratios
+        await definePlayerRatios()
+
+        # assign roles
+
+        players = list(self.players) #create separate list of players
+        while players != []:
+            if len(self.liberals) < self.liberalsNumber:
+                self.liberals += players.pop(random.randint(0,len(players)-1))
+            elif len(self.fascists) < self.fascistsNumber:
+                self.fascists += players.pop(random.randint(0,len(players)-1))
+        #select hitler at random from fascists
+        self.hitler = self.fascists[random.randint(0,len(players)-1)]
+
+
+    async def isRecent(self):
+        # Return false if game is idle for > 10 minutes
+        return time.time() - self.events[-1][1] < shGame.gameTimeout
+
+    async def addPlayer(self, message):
+        if message.author not in self.players:
+            self.players += [message.author]
+            overwrite = discord.PermissionOverwrite()
+            overwrite.read_messages = True
+            overwrite.send_message = not self.inProgress
+            await self.client.edit_channel_permissions(self.channel, message.author, overwrite)
+
+    #remove all secret hitler game instances in the server which have non-recent last events
+    async def shCleanup(client,message):
+        cleaned = False
+        game = await shGame.getGame(message)
+        if game:
+            # await client.delete_channel(game.channel)
+            cleaned = True
+        for c in message.server.channels:
+            if c.name == 'secret-hitler-game':
+                await client.delete_channel(c)
+                cleaned = True
+                break
+        # if cleaned and message: # if the channel still exists
+        #     await client.send_message(message.channel, "Cleaned up.")
+
+    async def removePlayer(self, message):
+        if message.author in self.players:
+            self.players.remove(message.author)
+            overwrite = discord.PermissionOverwrite()
+            overwrite.read_messages = False
+            overwrite.send_message = False
+            await self.client.edit_channel_permissions(self.channel, message.author, overwrite)
+            msg = "{} has left the game.".format(message.author)
+            await self.client.send_message(self.channel, msg)
+
+    async def getGame(message):
+        for game in shGame.gameServers:
+            if game.server == message.server:
+                return game
+        return []
+
 
     async def definePlayerRatios(self):
-        fascistsNumber = len(players) // 2 - 1
-        liberalsNumber = len(players) - fascistsNumber
+        self.fascistsNumber = len(players) // 2 - 1
+        self.liberalsNumber = len(players) - self.fascistsNumber
         # Randomly add a number of players to each team, according to these ratios
 
+
     async def announceGame(self,message):
-        announcemsg = """A Secret Hitler game is starting! \nType '!sh-join' to join in! """
+        announcemsg = """A Secret Hitler game is starting! \nType **sh-join** to join in! """
         await self.client.send_message(message.channel,announcemsg)
-        newgamemsg = """Welcome to Secret Hitler, \nOnce everybody's in, type \n'!sh-start' to start the game! """
+        newgamemsg = """Welcome to Secret Hitler, \nOnce everybody's in, type \n**sh-begin** to start the game! """
         await self.client.send_message(self.channel,newgamemsg)
+
+
+
+
+    #helper classes
+
+    class Policy:
+        def __init__(self,team):
+            self.team = team # Team is a number - 0 for fascist, 1 for liberal
+
+            def __repr__(self):
+                if team == 1:
+                    return "Liberal"
+                elif team == 0:
+                    return "Fascist"
+                else:
+                    return "Error"
+
+    class Pile:
+        def __init__(self):
+            self.policies = []
+        def shuffle(self):
+            newPile = []
+            for p in policies:
+                index = random.randint(0, len(self.policies)-1)
+                newPile += policies[index]
+            self.policies = newPile
+        def combine(self,other):
+            self.policies += other.policies
+            other.policies = []
+            self.shuffle()
+
+
+    class Player:
+        def __init__(self, user):
+            self.team = "none"
+            self.vote = "none"
+            self.player = user
+        def __repr__(self):
+            return self.player
--- mybot.py
+++ mybot.py
@@ -10,43 +10,85 @@ async def on_message(message):
         return
 
     # Check if the bot is working ---- Switch to "help" function?
-    if message.content.startswith('!hello'):
-        msg = 'Hello {0.author.mention}'.format(message)
+    if message.content.startswith('sh-help'):
+        msg = '''Welcome to Secret Hitler!\nCommands:
+    **sh-start**: Start a game
+    **sh-cleanup**: end current game (admin)
+    **sh-players**: display who's in a game right now
+    **sh-join**: join a lobby or spectate a game in progress
+    **sh-leave**: leave a game in progress
+    **sh-begin**: begin a game from the game lobby
+Notes:
+
+    * There is a 5 minute game timeout - if 5 minutes pass between game actions, a new game can be started!
+
+    * 5 players minimum per game
+
+    * One game per server! '''
         await client.send_message(message.channel, msg)
 
     # Start a game
-    if message.content.startswith('!sh-start'):
-        server = message.author.server
+    if message.content.startswith('sh-start'):
         await shStart(message)
 
-    # Clean up empty servers, to allow the start of a new game
-    if message.content.startswith('!sh-cleanup'):
-        if message.author.server_permissions.administrator:
-            await shCleanup(message.author.server)
-            for game in shGame.gameServers:
-                if game.server == message.server:
-                    await client.delete_channel(game.channel)
-                    await client.send_message(message.channel, "Cleaned up!")
+    if message.content.startswith('sh-begin'):
+        game = await shGame.getGame(message)
+        if game and message.channel == game.channel:
+            if len(game.players) > 4:
+                await game.startGame()
+            else:
+                await client.send_message(message.channel, "5 player minimum for a game")
 
-        # https://stackoverflow.com/questions/48141407/how-would-i-make-my-python-3-6-1-discord-bot-create-a-new-text-channel-in-a-serv
+    # Clean up empty servers, to allow the start of a new game
+    if message.content.startswith('sh-cleanup'):
+        if message.author.server_permissions.administrator:
+            await shGame.shCleanup(client,message)
+        else: # User is not an admin
+            await client.send_message(message.channel, "You need admin permissions to do this.")
+
+    if message.content.startswith('sh-players'):
+        game = await shGame.getGame(message)
+        if game:
+            await displayPlayers(message)
+
+    if message.content.startswith('sh-join'):
+        game = await shGame.getGame(message)
+        if game:
+            await game.addPlayer(message)
+
+    if message.content.startswith('sh-leave'):
+        game = await shGame.getGame(message)
+        if game:
+            await game.removePlayer(message)
+
 
 async def shStart(message):
-    # start a game of secret hitler, and add it to the gameServers list
-    server = message.author.server
-    everyone = discord.PermissionOverwrite(read_messages=False)
-    player = discord.PermissionOverwrite(read_messages=True)
-    for c in server.channels:
-        if c.name == 'secret-hitler-game':
-            await client.delete_channel(c)
-            break
-    channel = await client.create_channel(server, 'secret-hitler-game', (server.default_role, everyone), (server.me, player))
-    thisGame = shGame(client, message, channel)
-    shGame.gameServers += [thisGame]
-    await thisGame.announceGame(message)
+    # Check if game will start
+    gameWillStart = True
+    game = await shGame.getGame(message)
+    if game and await game.isRecent():
+        gameWillStart = False
 
-async def shCleanup(server):
-    check = True
-    #remove all secret hitler game instances in the server which have non-recent last events
+    if gameWillStart: # Start game
+        await shGame.createGame(client,message)
+
+    else: # Game in progress was found on this server
+        await client.send_message(message.channel, "There is a game in progress in this server. \nType **sh-join** to spectate! \nType **sh-help** for more options!")
+
+
+async def displayPlayers(message):
+    game = await shGame.getGame(message)
+    if game:
+        if game.players is not []:
+            msg = 'The players are'
+            for player in game.players:
+                msg += ", " + player.name
+            msg += '.'
+            await client.send_message(message.channel, msg)
+        else:
+            await client.send_message(message.channel, "No players found.")
+    else:
+        await client.send_message(message.channel, "No game was found on this server.")
 
 @client.event
 async def on_ready():
